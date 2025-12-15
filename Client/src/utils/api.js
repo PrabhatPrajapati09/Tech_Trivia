eeb8d21b402761d744d2d6adae29fe1630bf2c7d
PrabhatPrajapati09 <prabhatprajapati0099@gmail.com>
2025-12-15 09:54:41 +0530

    configure frontend for hosting

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_URL ,
 });
 
 // Add a request interceptor to include the token in headers
